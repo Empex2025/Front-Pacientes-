ae0f0a3aa848722b4d429e36a082cd6292b61fff
GabrielJose <gabrieljoseroberto@hotmail.com>
2025-08-19 17:27:53 -0300

    Update BottomNav.jsx

--- src/BottomNav.jsx
+++ src/BottomNav.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-// Importações aprimoradas
-import { TAB_CONFIG } from './utils/tab.config'; // Ajuste o caminho
-import { AnimatedTabBarIcon } from './animations/AnimatedTabBarIcon'; // Ajuste o caminho
+import { TAB_CONFIG } from './utils/tab.config';
+import { AnimatedTabBarIcon } from './animations/AnimatedTabBarIcon';
 
 const Tab = createBottomTabNavigator();
 
@@ -17,17 +16,10 @@ export const BottomNav = () => {
         tabBarStyle: styles.tabBar,
         tabBarLabelStyle: styles.tabBarLabel,
         tabBarInactiveTintColor: '#202124',
-        tabBarActiveTintColor: '#ffffff', // Cor do ícone/texto ativo
-        
-        // CORREÇÃO: Usamos tabBarActiveBackgroundColor para o item focado
-        // E tabBarInactiveBackgroundColor para os itens não focados.
-        // A propriedade tabBarItemStyle é para estilos estáticos, como o borderRadius.]h
-        
-        tabBarActiveBackgroundColor: '#d9eefcff', // Cor de fundo do item focado
-        tabBarInactiveBackgroundColor: '#ffffff', // Cor de fundo dos itens não focados
+        tabBarActiveTintColor: '#ffffff',
+        tabBarActiveBackgroundColor: '#d9eefcff',
+        tabBarInactiveBackgroundColor: '#ffffff',
         tabBarItemStyle: styles.tabBarItem,
-       // Aplicamos o estilo para bordas arredondadas e altura
-        
         tabBarIcon: ({ focused }) => {
           const { active, inactive } = TAB_CONFIG[route.name].icons;
           return (
@@ -40,7 +32,6 @@ export const BottomNav = () => {
         },
       })}
     >
-      {/* Mapeia a configuração para criar as telas dinamicamente */}
       {Object.entries(TAB_CONFIG).map(([name, config]) => (
         <Tab.Screen
           key={name}
@@ -54,28 +45,24 @@ export const BottomNav = () => {
 
 const styles = StyleSheet.create({
   tabBar: {
-    height: 90, // Altura total da barra
-    paddingTop: 10, // Espaço superior
-    borderRadius: 25, // Bordas arredondadas
+    height: 90,
+    paddingTop: 10,
+    borderRadius: 25,
     borderTopWidth: 0,
-    position: 'absolute', // Faz a barra flutuar // Distância da parte inferior da tela
- // Distância da direita
+    position: 'absolute',
     backgroundColor: '#ffffff',
-    // Sombras para dar o efeito de flutuação
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 4 },
     shadowOpacity: 0.1,
     shadowRadius: 8,
-
   },
   tabBarItem: {
-    borderRadius: 16, // Bordas arredondadas para o item
-    marginHorizontal: 5, // Espaço entre os itens
+    borderRadius: 16,
+    marginHorizontal: 5,
   },
   tabBarLabel: {
     fontSize: 12,
     fontWeight: '500',
     marginTop: 4,
-    // Pequeno espaço entre o ícone e o texto
   },
 });
