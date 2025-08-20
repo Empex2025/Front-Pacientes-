2de928cef7f0c6468eaac774c36cca249f6b6a84
GabrielJose <gabrieljoseroberto@hotmail.com>
2025-08-19 21:27:38 -0300

    criaçao do feedContext

--- src/components/FeedContext.jsx
+++ src/components/FeedContext.jsx
@@ -3,8 +3,6 @@ import React, { createContext, useContext, useState } from 'react';
 export const FeedContext = createContext();
 
 export const FeedProvider = ({ children }) => {
-  // false = mostra Feed.jsx (boas-vindas)
-  // true = mostra HomeFeed
   const [showHomeFeed, setShowHomeFeed] = useState(false);
 
   return (
